5b48150a17055a619240ebcd16c57810d34810a3
Hubert Mandeville <hubert.mandeville@nytimes.com>
2017-07-25 14:45:00 -0400

    updated

--- test.js
+++ test.js
@@ -12,7 +12,18 @@ $(function() {
         var box = "<div id=\"box" + i + "\"  class=\"box " + color + "\" data-color=\"" + color + "\"></div>";
         $("#boxes").append(box);        
         var top = Math.floor(Math.random() * 500);
-        var left = Math.floor(Math.random() * 800);
+        //var left = Math.floor(Math.random() * 800);
+        var left = 10;
+        if (color == "blue") {
+            left = 200;
+        } else if (color == "green") {
+            left = 400;
+        } else if (color == "red") {
+            left = 600;
+        } else if (color == "yellow") {
+            left = 800;
+        }
+
         $("#box" + i).css("top", top);
         $("#box" + i).css("left", left);
         $("#box" + i).css("z-index", i);
@@ -26,18 +37,42 @@ $(function() {
 
     $('.box').draggable(
         {
+            helper: draggableHelper,
             zIndex: 100,
-            revert: "invalid",
+            revert: true,
             cursor: "move",
             snap: ".color_box",
-            stack: ".green"
+            stack: "#contents"
         }
     );
 
     $('.color_box').droppable( {
+        accept: ".box",
+        hoverClass: 'hovered',
         drop: handleDropEvent
     });        
 });
+
+
+function draggableHelper() {
+    //console.log("in draggable helper");
+    var color = $(this).data("color");
+    var position = $(this).position();
+    var selected = $("." + color);
+    console.log(color + " selected at " + position.left + ", " + position.top);
+    //selected.css("position", "relative");
+    //selected.css("top", "");
+    //selected.css("left", "");    
+    //selected.css("top", position.top);
+    //selected.css("left", position.left);
+
+    var container = $('<div/>').attr('id', 'draggingContainer');
+    //container.css("top", position.top);
+    //container.css("left", position.left);
+    //selected.unwrap();
+    container.append(selected.clone());
+    return container;
+}
 
 
 function handleDropEvent( event, ui ) {
@@ -48,6 +83,15 @@ function handleDropEvent( event, ui ) {
     drag_color = draggable.data("color");
     drop_color = $(this).data("color")
     console.log( drag_color + " compare " + drop_color);
+
+
+    if (drag_color == drop_color) {
+        //ui.draggable.addClass( 'correct' );
+        ui.draggable.draggable( 'disable' );
+        //$(this).droppable( 'disable' );
+        ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+        ui.draggable.draggable( 'option', 'revert', false );
+    }     
 }
 
 
