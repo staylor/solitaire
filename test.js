b17b8ad9f3d736b0d8475f54b1ef7f17c1ca8529
Hubert Mandeville <hubert.mandeville@nytimes.com>
2017-07-25 17:59:39 -0400

    updated

--- test.js
+++ test.js
@@ -2,96 +2,127 @@
 //var colors = ["blue", "green", "orange", "pink", "purple", "red", "teal", "yellow"];
 var colors = ["blue", "green", "red", "yellow"];
 
+
+var selected = null;
+
 $(function() {
 
-    var num_boxes = Math.floor(Math.random() * 10) + 6;
+    //var num_boxes = Math.floor(Math.random() * 10) + 6;
+    var num_boxes = 10;
 
+    
     // setup boxes
     for (var i = 1; i <= num_boxes; i++) {
         var color = get_color();
-        var box = "<div id=\"box" + i + "\"  class=\"box " + color + "\" data-color=\"" + color + "\"></div>";
+        var box = "<div id=\"box" + i + "\" data-id=\"" + i + "\" class=\"box " + color + "\" data-color=\"" + color + "\" style=\"margin-left: " + (i*10) + "px\">" + i + "</div>";
         $("#boxes").append(box);        
-        var top = Math.floor(Math.random() * 500);
-        //var left = Math.floor(Math.random() * 800);
-        var left = 10;
-        if (color == "blue") {
-            left = 200;
-        } else if (color == "green") {
-            left = 400;
-        } else if (color == "red") {
-            left = 600;
-        } else if (color == "yellow") {
-            left = 800;
-        }
-
-        $("#box" + i).css("top", top);
-        $("#box" + i).css("left", left);
         $("#box" + i).css("z-index", i);
-    }
-
-    // set color boxes
-    for (var i = 0; i < colors.length; i++) {
-        var color_box = "<div id=\"color_" + colors[i] + "\" class=\"color_box\" data-color=\"" + colors[i] + "\">" + colors[i] + "</div>";
-        $("#colors").append(color_box);
     }
 
     $('.box').draggable(
         {
-            helper: draggableHelper,
-            zIndex: 100,
+            //revert: revertHandler,
             revert: true,
+            helper: helperHandler,
+            //drag: dragHandler,
+            start: startHandler,
+            stop: stopHandler,
+            zIndex: 100,
             cursor: "move",
-            snap: ".color_box",
+            //snap: "#drop_box",
             stack: "#contents"
         }
     );
 
-    $('.color_box').droppable( {
-        accept: ".box",
+    $('#drop_box').droppable( {
         hoverClass: 'hovered',
         drop: handleDropEvent
     });        
 });
 
 
-function draggableHelper() {
-    //console.log("in draggable helper");
-    var color = $(this).data("color");
-    var position = $(this).position();
-    var selected = $("." + color);
-    console.log(color + " selected at " + position.left + ", " + position.top);
-    //selected.css("position", "relative");
-    //selected.css("top", "");
-    //selected.css("left", "");    
-    //selected.css("top", position.top);
-    //selected.css("left", position.left);
+function dragHandler(event, ui) {
+    console.log("in drag handler: " + event.pageX + ", " + event.pageY);
+    //$("#container").css("margin-top", "-80px");
+    if (selected != null) {
+        selected.hide();
+        selected.remove();
+        selected = null;
+    }
+}
 
-    var container = $('<div/>').attr('id', 'draggingContainer');
-    //container.css("top", position.top);
-    //container.css("left", position.left);
-    //selected.unwrap();
-    container.append(selected.clone());
+function startHandler(event, ui) {
+    //console.log("in start handler");
+    if (selected != null) {
+        selected.hide();
+        //selected.remove();
+        //selected = null;
+    }
+}
+function stopHandler(event, ui) {
+    //console.log("in stop handler");
+    if (selected != null) {
+        selected.show();
+        $(".box").each(function() {
+            console.log("set z-index: " + $(this).data("id"));
+            $(this).css("z-index", $(this).data("id"));
+        });
+    }
+    
+}
+
+
+function helperHandler(event) {
+    //console.log("event pos: " + event.pageX + ", " + event.pageY);
+    var id = $(this).data("id");
+    console.log("selected: " + id);
+    var select_str = "";
+    var comma = "";
+    for (var i = id; i <= 10; i++) {        
+        select_str += comma + "#box" + i;
+        comma = ", ";
+    }
+    console.log("selecting " + select_str);
+    selected = $(select_str);
+
+    var container = $('<div/>').attr('id', 'container').css("margin-top", "-80px");
+    container.append(selected.clone());    
     return container;
 }
 
 
+function revertHandler(ui) {
+    console.log("in revert handler ", event);
+    console.log(" ui ", ui);
+    if(ui == false){
+        // revert back 
+        //selected.show();
+        return true;
+    } else {
+        //selected.remove();
+        return false;
+    }
+    
+}
+
+
+
 function handleDropEvent( event, ui ) {
     var draggable = ui.draggable;
-    
+
     console.log(draggable.attr('id') + ' was dropped onto ' + $(this).attr("id"));
 
-    drag_color = draggable.data("color");
-    drop_color = $(this).data("color")
-    console.log( drag_color + " compare " + drop_color);
+    $("#container").remove();
+    selected.remove();
+    
+    $("#drop_box").html("DROPPED!");
+    
+    //ui.draggable.draggable( 'disable' );
+    //$(this).droppable( 'disable' );
+    //ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+    //ui.draggable.draggable( 'option', 'revert', false );
 
-
-    if (drag_color == drop_color) {
-        //ui.draggable.addClass( 'correct' );
-        ui.draggable.draggable( 'disable' );
-        //$(this).droppable( 'disable' );
-        ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
-        ui.draggable.draggable( 'option', 'revert', false );
-    }     
+    
 }
 
 
